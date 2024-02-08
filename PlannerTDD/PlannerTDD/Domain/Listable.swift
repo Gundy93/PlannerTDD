4fa0ef10dac605a7b6b07a4032cbdcee0fda92cd
Gundy <>
2024-02-08 16:48:44 +0900

    feat: Identifiable을 준수하는 요소의 목록을 관리하는 타입을 정의하는 프로토콜 Listable 정의

---
+++ PlannerTDD/PlannerTDD/Domain/Listable.swift
@@ -0,0 +1,40 @@
+//
+//  Listable.swift
+//  PlannerTDD
+//
+//  Created by Gundy on 2/8/24.
+//
+
+import Foundation
+
+protocol Listable {
+    
+    associatedtype Element: Identifiable
+    
+    var list: [Element] { get set }
+    
+    mutating func add(_ newElement: Element)
+    mutating func edit(_ element: Element)
+    mutating func delete(_ element: Element)
+}
+
+extension Listable {
+    
+    mutating func add(_ newElement: Element) {
+        guard list.contains(where: { $0.id == newElement.id }) == false else { return }
+        
+        list.append(newElement)
+    }
+    
+    mutating func edit(_ element: Element) {
+        guard let index = list.firstIndex(where: { $0.id == element.id }) else { return }
+        
+        list[index] = element
+    }
+    
+    mutating func delete(_ element: Element) {
+        guard let index = list.firstIndex(where: { $0.id == element.id }) else { return }
+        
+        list.remove(at:index)
+    }
+}
