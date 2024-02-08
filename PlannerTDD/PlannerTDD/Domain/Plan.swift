a6a448308ae6e5561c18dc2451802455daa4d106
Gundy <>
2024-02-08 16:47:39 +0900

    feat: 계획을 정의하는 Plan 구조체 정의

---
+++ PlannerTDD/PlannerTDD/Domain/Plan.swift
@@ -0,0 +1,23 @@
+//
+//  Plan.swift
+//  PlannerTDD
+//
+//  Created by Gundy on 2/8/24.
+//
+
+import Foundation
+
+struct Plan: Identifiable {
+    
+    enum State {
+        case toDo
+        case doing
+        case done
+    }
+    
+    let id: UUID
+    var title: String
+    var deadline: Date
+    var description: String
+    var state: State
+}
